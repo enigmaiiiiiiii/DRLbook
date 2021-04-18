8670041249a0e1b8655634dfbe5feecc315c4be8
enigmaiiiiiiii <1213635698@qq.com>
2021-04-18 21:22:48 +0800

    初步判断：131行—135行执行效率较低

--- nature-DQN.py
+++ nature-DQN.py
@@ -14,7 +14,7 @@ import pickle
 
 writer = SummaryWriter('./losslog')
 
-BATCH_SIZE = 128
+BATCH_SIZE = 32
 LR = 0.001
 START_EPSILON = 1.0
 FINAL_EPSILON = 0.1
@@ -42,7 +42,7 @@ class Agent(object):
             checkpoint = torch.load(".\\TrainedAgent\\state.pth")
             self.network.load_state_dict(checkpoint['network'])
             self.target_network.load_state_dict(checkpoint['target_network'])
-            self.memory = pickle.load(open(".\\memory\\nature_DQN_memory", "wb"))
+            self.memory = pickle.load(open(".\\memory\\nature_DQN_memory", "wb"))  # 导入记忆
             # self.optimizer.load_state_dict(checkpoint['optimizer'])
             """只优化主网络参数"""
         self.learning_count = 0
@@ -78,12 +78,15 @@ class Agent(object):
         self.learning_count += 1
 
         batch = random.sample(self.memory, BATCH_SIZE)
+        """每次从100000个回忆中取出BATCH_SIZE个样本进行训练"""
+
+
         state = torch.FloatTensor([x[0] for x in batch]).to(device)
         action = torch.LongTensor([[x[1]] for x in batch]).to(device)
         reward = torch.FloatTensor([[x[2]] for x in batch]).to(device)
         next_state = torch.FloatTensor([x[3] for x in batch]).to(device)
         done = torch.FloatTensor([[x[4]] for x in batch]).to(device)
-        """每次从100000个回忆中取出32个样本进行训练"""
+        """tensor从cpu到GPU"""
         eval_q = self.network.forward(state).gather(1, action)  # 主网络Q
         """从主网络中返回动作价值"""
         next_q = self.target_network(next_state).detach()
@@ -119,13 +122,23 @@ class Agent(object):
             通过load_state_dict函数target_network复制主网络network模型参数"""
         self.learning_count += 1
 
+        start = time.time()  #
         batch = random.sample(self.memory, BATCH_SIZE)
+        end = time.time()
+        sample_time = end - start  #
+
+        start = time.time()  #
         state = torch.FloatTensor([x[0] for x in batch]).to(device)
         action = torch.LongTensor([[x[1]] for x in batch]).to(device)
         reward = torch.FloatTensor([[x[2]] for x in batch]).to(device)
         next_state = torch.FloatTensor([x[3] for x in batch]).to(device)
         done = torch.FloatTensor([[x[4]] for x in batch]).to(device)
+        """占用很长时间"""
+        end = time.time()  #
+        GPU_time = end - start
+
         """每次从100000个回忆中取出32个样本进行训练"""
+        start = time.time()  #
         eval_q = self.network.forward(state).gather(1, action)  # 主网络Q
         """从主网络中返回动作价值"""
         next_q = self.target_network(next_state).detach()
@@ -141,6 +154,9 @@ class Agent(object):
         self.optimizer.zero_grad()
         self.loss.backward()
         self.optimizer.step()
+        end = time.time()  #
+        train_time = end - start
+        # print("取样时间：{0:4f}\t转换时间：{1:4f}\t神经网络计算时间：{2:4f}".format(sample_time,GPU_time,train_time))
         if i_episode % 50 == 0:
             """每50局游戏保存一次参数"""
             train_state = {'network': self.network.state_dict(),
@@ -161,39 +177,44 @@ for i_episode in range(TOTAL_EPISODES):
     start_time = time.time()
     state = env.reset()
     state = preprocess(state)
+    total = 0
     """像素状态处理，压缩转置"""
     while True:
         """一方分数达到20，done为True,while循环结束"""
-        # env.render()
+        env.render()
         action = agent.action(state, True)
         next_state, reward, done, info = env.step(action)
 
         next_state = preprocess(next_state)
+        start = time.time()
         agent.hotlearn(state, action, reward, next_state, done, i_episode)
-        if reward == 1:
-            """
-            对于Pong-v0这个游戏，state是像素，得分直接用作reward
-            将图片数据裁剪，转置成模型输入数据tensor
-            """
-            reward_num += 1
-            print("每得1分用时{}秒".format(time.time() - reward_time))  # 每次得分的耗时间隔
-            writer.add_scalar("get_1_score_time_cost", time.time() - reward_time, reward_num)  # 将得分耗时间隔曲线记录在tensorboard
-            reward_time = time.time()  # 得分时刻
-
-            state_img = Image.fromarray(state.squeeze())
-            imageno += 1
-            image_path = os.path.join(".\\image\\", str(imageno) + ".jpg")
-            state_img.save(image_path, "jpeg")  # 将得分瞬间保存成图片
-
-            if agent.loss.size() != torch.Size([0]):
-                writer.add_scalar("loss(reward=1)", agent.loss.cpu().detach().numpy(), reward_num)
+        end = time.time()
+        method_time = end-start
+        total += method_time
+        # if reward == 1:
+        #     """
+        #     对于Pong-v0这个游戏，state是像素，得分直接用作reward
+        #     将图片数据裁剪，转置成模型输入数据tensor
+        #     """
+        #     reward_num += 1
+        #     print("每得1分用时{}秒".format(time.time() - reward_time))  # 每次得分的耗时间隔
+        #     writer.add_scalar("get_1_score_time_cost", time.time() - reward_time, reward_num)  # 将得分耗时间隔曲线记录在tensorboard
+        #     reward_time = time.time()  # 得分时刻
+        #
+        #     state_img = Image.fromarray(state.squeeze())
+        #     imageno += 1
+        #     image_path = os.path.join(".\\image\\", str(imageno) + ".jpg")
+        #     state_img.save(image_path, "jpeg")  # 将得分瞬间保存成图片
+        #
+        #     if agent.loss.size() != torch.Size([0]):
+        #         writer.add_scalar("loss(reward=1)", agent.loss.cpu().detach().numpy(), reward_num)
 
         state = next_state
         if done:
             break
     time_1 = time.time() - start_time
     writer.add_scalar("time_1", time_1, i_episode)
-    # print("训练时间：{:8f} ".format(time_1))
+    print("训练时间总时间：{0:.4f}\thotlearn方法总时间：{1:.4f}\t百分比：{2:.2f}%".format(time_1,total,total/time_1*100))
     if EPSILON > FINAL_EPSILON:
         EPSILON -= (START_EPSILON - FINAL_EPSILON) / EXPLORE
 
